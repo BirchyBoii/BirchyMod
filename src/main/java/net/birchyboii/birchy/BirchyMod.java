6fa9b3bcf9c8fa8a4b175e2c80e4201ab27e265b
BirchyBoii <bughioazlaan09@gmail.com>
2026-09-16 17:38:00 +0500

    Initial commit

---
+++ src/main/java/net/birchyboii/birchy/BirchyMod.java
@@ -0,0 +1,18 @@
+package net.birchyboii.birchy;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BirchyMod implements ModInitializer {
+	public static final String MOD_ID = "birchy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/birchyboii/birchy/BirchyModClient.java
@@ -0,0 +1,10 @@
+package net.birchyboii.birchy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BirchyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
